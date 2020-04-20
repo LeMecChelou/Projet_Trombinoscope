9718b73b417e9d10825955986328801d4b1f957d
LeMecChelou <snow.benjamin2@gmail.com>
2020-04-20 15:23:02 +0200

    Grosse modification d'arborescence + modification du CSS (1 -> 2: 1 par site maintenant). Ajout de formulaires dans la page étudiante. Modification de l'image de background (compression).

--- Trombinoscope/scripts/Javascript/display_form.js
+++ Trombinoscope/scripts/Javascript/display_form.js
@@ -1,26 +1,34 @@
 function connection_priority(){
-    let co = document.getElementsByClassName("bouton_choix_etu")[0];
-    let ins = document.getElementsByClassName("bouton_choix_etu")[1];
+    let co = document.getElementsByClassName("bouton_choix")[0];
+    let ins = document.getElementsByClassName("bouton_choix")[1];
 
-    if (co.id === "choix_etu_inactif"){
-        co.id = "choix_etu_actif";
-        ins.id = "choix_etu_inactif";
+    if (co.id === "choix_inactif"){
+        co.id = "choix_actif";
+        ins.id = "choix_inactif";
 
-        let formulaire = document.getElementById("formulaire_etu");
-        formulaire.innerHTML = "";
+        let formulaire = document.getElementById("formulaire");
+        formulaire.innerHTML = "<form method=\"post\" action=\"../scripts/PHP/check_connection.php\">\n" +
+            "                    <label class=\"input_label\" for=\"input_id\">Identifiant</label>\n" +
+            "                    <input type=\"text\" id=\"input_id\" class=\"input_co\" name=\"input_id\"/>\n" +
+            "\n" +
+            "                    <label class=\"input_label\" for=\"input_passwd\">Mot de passe</label>\n" +
+            "                    <input type=\"password\" id=\"input_passwd\" class=\"input_co\" name=\"input_passwd\"/>\n" +
+            "\n" +
+            "                    <input type=\"button\" id=\"bouton_submit_co\" value=\"Se connecter\" onclick=\"checkConnection();\"/>\n" +
+            "                </form>";
     }
 }
 
 
 function inscription_priority(){
-    let co = document.getElementsByClassName("bouton_choix_etu")[0];
-    let ins = document.getElementsByClassName("bouton_choix_etu")[1];
+    let co = document.getElementsByClassName("bouton_choix")[0];
+    let ins = document.getElementsByClassName("bouton_choix")[1];
 
-    if (ins.id === "choix_etu_inactif") {
-        co.id = "choix_etu_inactif";
-        ins.id = "choix_etu_actif";
+    if (ins.id === "choix_inactif") {
+        co.id = "choix_inactif";
+        ins.id = "choix_actif";
 
-        let formulaire = document.getElementById("formulaire_etu");
+        let formulaire = document.getElementById("formulaire");
         formulaire.innerHTML = "";
     }
 }
