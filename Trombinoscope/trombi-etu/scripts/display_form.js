748a94d09876d74bcd2d0086568791b919dcb978
LeMecChelou <snow.benjamin2@gmail.com>
2020-04-20 12:11:41 +0200

    Maj du 20/04/2020 : 12:11

---
+++ Trombinoscope/trombi-etu/scripts/display_form.js
@@ -0,0 +1,25 @@
+function connection_priority(){
+    let co = document.getElementsByClassName("bouton_choix_etu")[0];
+    let ins = document.getElementsByClassName("bouton_choix_etu")[1];
+
+    if (co.id === "choix_etu_inactif"){
+        co.id = "choix_etu_actif";
+        ins.id = "choix_etu_inactif";
+
+        let formulaire = document.getElementById("formulaire_etu");
+        formulaire.innerHTML = ";"
+    }
+}
+
+
+function inscription_priority(){
+    let co = document.getElementsByClassName("bouton_choix_etu")[0];
+    let ins = document.getElementsByClassName("bouton_choix_etu")[1];
+
+    if (ins.id === "choix_etu_inactif") {
+        co.id = "choix_etu_inactif";
+        ins.id = "choix_etu_actif";
+
+        let formulaire = document.getElementById("formulaire_etu");
+    }
+}
