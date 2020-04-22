380373a6087a970d8e5e26a20fcd4d0032a12562
LeMecChelou <snow.benjamin2@gmail.com>
2020-04-22 13:10:49 +0200

    Affichage des informations sur la page etudiant.php.

--- Trombinoscope/trombi-etu/etudiant.php
+++ Trombinoscope/trombi-etu/etudiant.php
@@ -18,7 +18,51 @@
         </header>
 
         <div id="conteneur_infos_etu">
+            <div id="conteneur_infos">
+                <div id="div_table_info_etu">
+                    <table id="table_info_etu">
+                        <?php
+                            session_start();
+                            if (isset($_SESSION['token'])){
+                                $id = $_SESSION['token'];
+                                $id = explode(';', $id)[0];
 
+                                $fichier = file("./files/etudiants.csv");
+                                for ($k = 0; $k < sizeof($fichier); $k++){
+                                    $ligne = str_replace("\n", "", $fichier[$k]);
+                                    $ligne = explode(";", $ligne);
+
+                                    if ($id == $ligne[0]){
+                                        if ($ligne[9] == "None"){
+                                            echo "<img id='error' class='image_profil' src='./assets/pp_none.png' alt=\"Image de profil d'erreur.\"/>";
+                                        }
+                                        else{
+                                            echo "<img id='None' class='image_profil' src='$ligne[9]' alt=\"Image de profil.\"/>";
+                                        }
+
+                                        echo "<p id='erreur_image_profil'></p>";
+
+                                        echo "<tr><td class='colonne_titre'>Identifiant:</td><td>$ligne[0]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Prénom:</td><td>$ligne[1]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Nom:</td><td>$ligne[2]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Filière:</td><td>$ligne[3]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Groupe:</td><td>$ligne[4]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Mail:</td><td>$ligne[5]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Numéro:</td><td>$ligne[6]</td></tr>";
+                                        echo "<tr><td class='colonne_titre'>Adresse:</td><td>$ligne[7]</td></tr>";
+
+                                    }
+                                }
+                            }
+                            else{
+                                header('Location: ./index.php');
+                            }
+
+                        ?>
+                    </table>
+                    <button id="bouton_change_infos" onclick=''>Changer les informations</button>
+                </div>
+            </div>
         </div>
 
     </body>
