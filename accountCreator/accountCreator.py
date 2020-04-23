fead9591ed254e131b6544e835872467ab9be4b4
LeMecChelou <snow.benjamin2@gmail.com>
2020-04-23 11:21:09 +0200

    Commit de accountCreator après avoir compris que je n'étais pas dans le bon dossier.

---
+++ accountCreator/accountCreator.py
@@ -0,0 +1,41 @@
+import random
+import hashlib
+from setup import *
+
+
+def createPhone():
+
+    phone = "06"
+    for k in range(4):
+        digits = random.randint(10, 99)
+        phone += "." + str(digits)
+
+    return phone
+
+
+def saveAccount(account):
+
+    with open("comptes.csv", "a") as fichier:
+        fichier.write(account)
+
+
+nb_comptes = int(input("Entrez le nombre de comptes à créer: "))
+
+m_hash = hashlib.sha256()
+
+for nb in range(nb_comptes):
+    prenom = random.choice(PRENOMS)
+    nom = random.choice(NOMS)
+    filiere = random.choice(FILIERES)
+    groupe = random.choice(GROUPES[filiere])
+    phone = createPhone()
+
+    idt = (prenom[0] + nom).lower()
+    mail = (prenom + "." + nom + "@gmail.com").lower()
+
+    mdp = (nom[0] + prenom).lower().encode()
+    m_hash.update(mdp)
+
+    compte = "{};{};{};{};{};{};{};{};{}\n".format(idt, prenom, nom, filiere, groupe, mail, phone, m_hash.hexdigest(),
+                                                   DIR_PP)
+    saveAccount(compte)
---
+++ accountCreator/setup.py
@@ -0,0 +1,21 @@
+PRENOMS = ["Thibault", "Joseph", "Marie", "Jesus", "Marine", "Quentin", "Ariana", "Florian", "Merle", "Gilbert", "Jean",
+           "Patrick", "Bernard", "Benjamin", "Pierre", "Nathan", "Killian", "Nour", "Eden", "Joel", "Martine", "Martin",
+           "Nicolas", "Francois", "Emmanuel", "Charles", "Jacque", "Jacqueline", "Valerie", "Georges", "Jose", "Robert",
+           "Arthur", "Etienne", "Jean-Remi", "Serge", "Remi", "Theophane", "Thimote", "Clemence", "Damien", "Constance",
+           "Pierre", "Robin", "Claire", "Sophie", "Caroline", "Diana", "Elene", "Tiara", "Christophe", "Christine",
+           "Chrisitina", "Christopher", "Philippe", "Delphine", "Luis", "Maurice", "Markus", "Stirling", "Bob", "Paul",
+           "Jhon", "Louis", "Henry", "Victor", "Guy", "Hans", "Jean-Francois", "Bruce", "Peter", "Pablo", "Ellis"]
+
+NOMS = ["Thiers", "Macron", "Hollande", "Grevy", "Carnot", "Casimir-Perier", "Faure", "Loubet", "de Gaulle", "Pompidou",
+        "Giscard d'Estaing", "Mitterrand", "Chirac", "Sarkozy", "Hollande", "Auriol", "Coty", "Millerand", "Doumergue",
+        "Doumer", "Lebrun", "Deschanel", "Poincare", "Fallieres", "Gainsbourg", "Gaillard", "Dennehy", "Barrier",
+        "Brassens", "Nahon", "Serina", "Sepulveda", "Chaplin", "Stalter", "Raetz", "Moss", "Benichou", "Marley", "Haddad",
+        "Prine", "Calin", "Calvet", "Hugo", "Calvet", "Calvin", "Williams", "Garcin", "Rosny", "Meyer", "Cesarini",
+        "Bourvil", "Hallyday", "Lee", "Bazin", "Whittingham", "Drucker", "Escobar", "Marsalis", "Curtis", "Portal"]
+
+FILIERES = ['L1-MIPI', "L2-MI", "L3-I", "LP RS", "LPI-RIWS"]
+
+GROUPES = {"L1-MIPI": ["A1", "A2", "A3"], "L2-MI": ['B1', "B2", "B3"], "L3-I": ['C1', 'C2', 'C3'],
+           "LP RS": ['D1', 'D2', 'D3'], "LPI-RIWS": ['E1', 'E2', 'E3']}
+
+DIR_PP = "None"
